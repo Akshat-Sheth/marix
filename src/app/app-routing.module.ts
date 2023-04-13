6eb8aece99ae42b037b46dd03bf89d3ff72bb72d
Akshat Sheth <akshat.sheth@pmcretail.com>
2023-04-13 17:13:14 +0530

    dashboard setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const authModule = () => import('./authentication/authentication.module').then(x => x.AuthenticationModule);
+const dashboardModule = () => import('./dashboard/dashboard.module').then(x => x.DashboardModule);
 const routes: Routes = [{
   path: '', loadChildren:authModule
+},
+{
+  path:'dashboard', loadChildren:dashboardModule
 }];
 
 @NgModule({
