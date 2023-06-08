24cdbbf69dcb7822b61cd50c4140a19b4b4b848e
Akshat Sheth <akshat.sheth@pmcretail.com>
2023-06-08 16:51:24 +0530

    share bug fixed

--- src/app/dashboard/share/share.component.ts
+++ src/app/dashboard/share/share.component.ts
@@ -12,7 +12,56 @@ export class ShareComponent implements OnInit {
   ngOnInit() {
   }
 
+  everyOneSelected = false
+
   formVisible = false
+  
+  employeeList = [
+    {
+      id:1,
+      name:"akshat",
+      department:"Cloud",
+      selected:false
+    },
+    {
+      id:2,
+      name:"Harsh",
+      department:"Cloud",
+      selected:false
+    },
+    {
+      id:3,
+      name:"Neel",
+      department:"Hybrid",
+      selected:false
+    },
+    {
+      id:4,
+      name:"freya",
+      department:"Hybrid",
+      selected:false
+    },
+    {
+      id:5,
+      name:"hetvi",
+      department:"Hybrid",
+      selected:false
+    },
+    {
+      id:6,
+      name:"Hardik",
+      department:"Android",
+      selected:false
+    },
+    {
+      id:7,
+      name:"Anushka",
+      department:"IOS",
+      selected:false
+    }
+  ]
+
+  temp = this.employeeList.map(ele => ele)
 
   generateLink(){
     this.formVisible = true
@@ -20,6 +69,18 @@ export class ShareComponent implements OnInit {
 
   cancel(){
     this.formVisible = false
+  }
+
+  share(){
+    let final:any[] = []
+
+    this.temp.map(ele =>{
+      if(ele.selected){
+        final.push(ele)
+      }
+    })
+
+    console.log(final)
   }
 
   onSearch(value:any){
@@ -31,51 +92,29 @@ export class ShareComponent implements OnInit {
   }
 
   selectAll(){
-    // console.log(value)
-    console.log("hello")
+
+    if(this.everyOneSelected){
+      this.temp.map(ele => {
+        ele.selected = false
+      })
+      this.everyOneSelected = false
+    }else{
+      this.temp.map(ele => {
+        ele.selected = true
+      })
+      this.everyOneSelected = true
+    }
   }
 
-  
+  checked(value:any){
+    this.temp.map(ele =>  {
+      if(ele.name === value.name){
+        ele.selected = true
+      }
+    })
+    console.log(this.temp)
+  }
 
-  employeeList = [
-    {
-      id:1,
-      name:"akshat",
-      department:"Cloud"
-    },
-    {
-      id:2,
-      name:"Harsh",
-      department:"Cloud"
-    },
-    {
-      id:3,
-      name:"Neel",
-      department:"Hybrid"
-    },
-    {
-      id:4,
-      name:"freya",
-      department:"Hybrid"
-    },
-    {
-      id:5,
-      name:"hetvi",
-      department:"Hybrid"
-    },
-    {
-      id:6,
-      name:"Hardik",
-      department:"Android"
-    },
-    {
-      id:7,
-      name:"Anushka",
-      department:"IOS"
-    }
-  ]
-
-  temp = this.employeeList.map(ele => ele)
 
 
 
