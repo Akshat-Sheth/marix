2f7174ab7d40c7ca5c7599139dcc3bc4de7d9385
Akshat Sheth <akshat.sheth@pmcretail.com>
2023-06-08 11:57:09 +0530

    share part

--- src/app/dashboard/share/share.component.ts
+++ src/app/dashboard/share/share.component.ts
@@ -22,4 +22,61 @@ export class ShareComponent implements OnInit {
     this.formVisible = false
   }
 
+  onSearch(value:any){
+    console.log(value.value)
+    if(value.value.length === 0){
+      this.temp = this.employeeList
+    }
+    this.temp = this.employeeList.filter(ele => ele.department.toLowerCase().startsWith(value.value))
+  }
+
+  selectAll(){
+    // console.log(value)
+    console.log("hello")
+  }
+
+  
+
+  employeeList = [
+    {
+      id:1,
+      name:"akshat",
+      department:"Cloud"
+    },
+    {
+      id:2,
+      name:"Harsh",
+      department:"Cloud"
+    },
+    {
+      id:3,
+      name:"Neel",
+      department:"Hybrid"
+    },
+    {
+      id:4,
+      name:"freya",
+      department:"Hybrid"
+    },
+    {
+      id:5,
+      name:"hetvi",
+      department:"Hybrid"
+    },
+    {
+      id:6,
+      name:"Hardik",
+      department:"Android"
+    },
+    {
+      id:7,
+      name:"Anushka",
+      department:"IOS"
+    }
+  ]
+
+  temp = this.employeeList.map(ele => ele)
+
+
+
 }
